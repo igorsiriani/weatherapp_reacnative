492c2567b7e36163b7b1377dd3c86d4eb753a006
Igor <igorsiriani@gmail.com>
2021-12-12 10:34:55 -0300

    First Commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Today from './components/today';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Today weather={true} temperature={true} />
     </View>
   );
 }
@@ -14,8 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F7F7FF',
     alignItems: 'center',
-    justifyContent: 'center',
   },
 });
